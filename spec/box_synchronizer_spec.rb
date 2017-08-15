3f5885498b7d8956cac7cd0bfbfd288fefc8f640
Jen <jvarelalijo@gmail.com>
2017-08-15 12:14:25 +0200

    specs

--- spec/box_synchronizer_spec.rb
+++ spec/box_synchronizer_spec.rb
@@ -1,4 +1,4 @@
-describe ::DiscourseBackupToNextcloud::NextcloudSynchronizer do
+describe ::DiscourseBackupToBox::BoxSynchronizer do
 
   let(:backup) { Backup.new('backup') }
 
@@ -11,29 +11,29 @@ describe ::DiscourseBackupToNextcloud::NextcloudSynchronizer do
 
   describe "#can_sync?" do
     it "should return false when disabled via site setting" do
-      SiteSetting.discourse_sync_to_nextcloud_enabled = false
-      SiteSetting.discourse_sync_to_nextcloud_api_key = 'test_key'
+      SiteSetting.discourse_sync_to_box_enabled = false
+      SiteSetting.discourse_sync_to_box_api_key = 'test_key'
       ds = described_class.new(backup)
       expect(ds.can_sync?).to eq(false)
     end
 
     it "should return false when the backup is missing" do
-      SiteSetting.discourse_sync_to_nextcloud_enabled = true
-      SiteSetting.discourse_sync_to_nextcloud_api_key = 'test_key'
+      SiteSetting.discourse_sync_to_box_enabled = true
+      SiteSetting.discourse_sync_to_box_api_key = 'test_key'
       ds = described_class.new(nil)
       expect(ds.can_sync?).to eq(false)
     end
 
     it "should return false when the api key is missing" do
-      SiteSetting.discourse_sync_to_nextcloud_enabled = true
-      SiteSetting.discourse_sync_to_nextcloud_api_key = ''
+      SiteSetting.discourse_sync_to_box_enabled = true
+      SiteSetting.discourse_sync_to_box_api_key = ''
       ds = described_class.new(backup)
       expect(ds.can_sync?).to eq(false)
     end
 
     it "should return true when everything is correct" do
-      SiteSetting.discourse_sync_to_nextcloud_enabled = true
-      SiteSetting.discourse_sync_to_nextcloud_api_key = 'test_key'
+      SiteSetting.discourse_sync_to_box_enabled = true
+      SiteSetting.discourse_sync_to_box_api_key = 'test_key'
       ds = described_class.new(backup)
       expect(ds.can_sync?).to eq(true)
     end
